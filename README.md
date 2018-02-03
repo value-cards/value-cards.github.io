b21efb1dd72ce185b070fef0abf960beea635f4b
Kit Brown-Watts <kbwatts@gmail.com>
2018-02-02 23:30:12 -0800

    Example working template
    Minimal code example for dynamically templating team roadmaps

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # value-cards.github.io
+
+This is a minimal app that renders initiatives in a simple way.
---
+++ js/sample.js
@@ -0,0 +1,47 @@
+var initiatives = [
+  "Initiative 1",
+  "Initiative 2",
+  "Initiative 3",
+  "Initiative 4"
+];
+var descriptions = [
+  "Initiative Description 1",
+  "Initiative Description 2",
+  "Initiative Description 3",
+  "Initiative Description 4"
+];
+var progressValues = [
+  "0",
+  "0",
+  "0",
+  "0"
+];
+var primaryKPIs = [
+  "+40%",
+  "+$100K",
+  "+$250K",
+  "+$500K",
+  "+$600K"
+]
+var primaryMetrics = [
+  "re-engagement rate",
+  "revenue",
+  "revenue",
+  "revenue",
+  "revenue"
+]
+var secondaryKPIs = [
+  "-10%",
+  "",
+  "",
+  "",
+  ""
+];
+
+var secondaryMetrics = [
+  "cart abandonment",
+  "",
+  "",
+  "",
+  ""
+]
